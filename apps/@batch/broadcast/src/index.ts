758ec4a731b239fe26132c000c868e9b267a8839
zuhno <zuhno11@gmail.com>
2023-12-04 16:42:40 +0900

    refactor: broadcast in batch

--- apps/@batch/broadcast/src/index.ts
+++ apps/@batch/broadcast/src/index.ts
@@ -1,30 +1,58 @@
-import { RecurrenceRule, scheduleJob } from "node-schedule";
+import { RecurrenceRule, gracefulShutdown, scheduleJob, scheduledJobs } from "node-schedule";
 import "dotenv/config";
 
 import { job } from "./schedule";
 import { IntervalTimeEnum } from "./types";
 
-const rule = new RecurrenceRule();
+const threeTimeJob = () => {
+  const rule = new RecurrenceRule();
 
-rule.hour = [2, 5, 8, 11, 14, 17, 20, 23];
-rule.minute = 55;
+  rule.hour = [9, 12, 15, 18, 21];
+  rule.minute = 0;
+
+  // Called every 3 hour
+  scheduleJob(rule, async () => {
+    await job(IntervalTimeEnum.THREE);
+  });
+};
+
+const sixTimeJob = () => {
+  const rule = new RecurrenceRule();
+
+  rule.hour = [9, 15, 21];
+  rule.minute = 0;
+
+  // Called every 6 hour
+  scheduleJob(rule, async () => {
+    await job(IntervalTimeEnum.SIX);
+  });
+};
+
+const twelveTimeJob = () => {
+  const rule = new RecurrenceRule();
+
+  rule.hour = [9, 21];
+  rule.minute = 0;
+
+  // Called every 12 hour
+  scheduleJob(rule, async () => {
+    await job(IntervalTimeEnum.TWELVE);
+  });
+};
 
 const main = () => {
   console.log("Scheduler is Running 🥳");
-  job(IntervalTimeEnum.THREE);
 
-  // // Called every 55 minutes of every hour
-  // scheduleJob(rule, () => {
-  //   job();
-  // });
+  threeTimeJob();
+  sixTimeJob();
+  twelveTimeJob();
 
-  // // Graceful Shutdown
-  // process.on("SIGINT", function () {
-  //   gracefulShutdown().then(() => {
-  //     console.log("succeed graceful shutdown 😎");
-  //     process.exit(0);
-  //   });
-  // });
+  process.on("SIGINT", function () {
+    gracefulShutdown().then(() => {
+      console.log("succeed graceful shutdown 😎");
+      process.exit(0);
+    });
+  });
 };
 
 main();
--- apps/@batch/scrap/src/index.ts
+++ apps/@batch/scrap/src/index.ts
@@ -3,17 +3,17 @@ import "dotenv/config";
 
 import { job } from "./schedule";
 
-const rule = new RecurrenceRule();
-
-rule.hour = [2, 5, 8, 11, 14, 17, 20, 23];
-rule.minute = 55;
-
 const main = () => {
+  const rule = new RecurrenceRule();
+
+  rule.hour = [2, 5, 8, 11, 14, 17, 20, 23];
+  rule.minute = 55;
+
   console.log("Scheduler is Running 🥳");
 
   // Called every 55 minutes of every hour
-  scheduleJob(rule, () => {
-    job();
+  scheduleJob(rule, async () => {
+    await job();
   });
 
   // Graceful Shutdown
