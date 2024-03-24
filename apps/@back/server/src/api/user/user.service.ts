c81fdf6ea4c216d61c87448d64eefa4a043d7f69
zuhno <zuhno11@gmail.com>
2024-03-24 10:59:23 +0900

    refactor: supabase type, remove unnecessary code

--- apps/@back/server/src/api/user/user.service.ts
+++ apps/@back/server/src/api/user/user.service.ts
@@ -16,21 +16,11 @@ export class UserService {
   async postLogout(accessToken?: string) {
     const decoded = await this._decoded(accessToken);
 
-    const exist = await this.supabaseService
-      .getClient()
-      .serviceRole.from("UserAuth")
-      .select("userId")
-      .eq("userId", decoded.id)
-      .limit(1)
-      .single();
-
-    if (exist.error) throw new HttpException(exist.error.message, HttpStatus.BAD_REQUEST);
-
     const user = await this.supabaseService
       .getClient()
       .serviceRole.from("UserAuth")
       .delete({ count: "exact" })
-      .eq("userId", exist.data.userId);
+      .eq("userId", decoded.id);
 
     if (user.error) throw new HttpException(user.error.message, HttpStatus.BAD_REQUEST);
 
--- packages/supabase-type/database.types.ts
+++ packages/supabase-type/database.types.ts
@@ -104,26 +104,20 @@ export type Database = {
             isOneToOne: false
             referencedRelation: "Category"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       FeedView: {
         Row: {
-          created_at: string
           id: string
-          updated_at: string | null
           view: number
         }
         Insert: {
-          created_at?: string
           id: string
-          updated_at?: string | null
           view?: number
         }
         Update: {
-          created_at?: string
           id?: string
-          updated_at?: string | null
           view?: number
         }
         Relationships: []
@@ -176,7 +170,7 @@ export type Database = {
             isOneToOne: false
             referencedRelation: "Interval"
             referencedColumns: ["time"]
-          }
+          },
         ]
       }
       Subscriber: {
@@ -243,7 +237,7 @@ export type Database = {
             isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       User: {
@@ -332,7 +326,7 @@ export type Database = {
             isOneToOne: true
             referencedRelation: "User"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
     }
@@ -340,25 +334,6 @@ export type Database = {
       [_ in never]: never
     }
     Functions: {
-      increment:
-        | {
-            Args: {
-              table_name: string
-              row_id: number
-              x: number
-              field_name: string
-            }
-            Returns: undefined
-          }
-        | {
-            Args: {
-              table_name: string
-              row_id: string
-              x: number
-              field_name: string
-            }
-            Returns: undefined
-          }
       increment_field: {
         Args: {
           table_name: string
@@ -373,7 +348,6 @@ export type Database = {
       AppFrom: "SLACK"
       DeactiveReason: "NOT_FOUND" | "USER_REQUEST" | "BAN" | "UNKOWN"
       MemberRole: "OWNER" | "ADMIN" | "USER_BASIC"
-      Reason: "NOT_FOUND" | "USER_REQUEST" | "UNKOWN"
       SocialPlatform: "GOOGLE"
     }
     CompositeTypes: {
@@ -382,14 +356,16 @@ export type Database = {
   }
 }
 
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
@@ -397,67 +373,67 @@ export type Tables<
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
+    | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
